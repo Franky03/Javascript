ac62c0d1210b64a87cb06e505b62f151d731d1f7
Frankley Kaiky <kaikym20@gmail.com>
2023-01-02 23:58:07 -0300

    strings

--- Udemy/Aula2/index.js
+++ Udemy/Aula2/index.js
@@ -11,9 +11,42 @@ console.log(varA, varB, varc);
 
 let aux;
 
-aux = varA;
-varA = varB;
-varB= varc;
-varc = aux;
+// aux = varA;
+// varA = varB;
+// varB= varc;
+// varc = aux;
+
+[varA, varB, varc] = [varB, varc, varA];
 
 console.log(varA, varB, varc);
+
+let string= "posso usar aspas \"ASSIM\" igual C, \\ \\ ";
+string= 'um texto um';
+
+
+// charAt é para pegar o elemento que está na posição X
+console.log(string.charAt(4));
+// concatenar strings
+console.log(string.concat(' concatenado'));
+// index de algo
+console.log(string.indexOf('um'));
+// index de algo a partir de um indice
+console.log(string.indexOf('um', 3));
+// começando do final 
+console.log(string.lastIndexOf('um'));
+// match e search (encontrar expressões regulares na string)
+console.log(string.match(/[a-z]/g));
+console.log(string.search(/[x]/g));
+// replace
+console.log(string.replace('um', 'outro'));
+string= 'o rato roeu a roupa do rei de roma.';
+console.log(string.replace(/r/g, 'l'));
+// tamanho 
+console.log(string.length);
+//slice da string
+console.log(string.slice(-5));
+console.log(string.slice(2,6));
+console.log(string.slice(-5, -1));
+//split
+let x=4;
+console.log(string.split(' ', x));
